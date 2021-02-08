5abf3aae524ca48dc0a790147916b539b3fb7542
Chris Johnson <chrisjohnson00@hotmail.com>
2021-02-07 21:15:02 -0800

    feat: implementing test and methods for getting audio tracks

--- README.md
+++ README.md
@@ -3,3 +3,26 @@
     pip install --upgrade kafka-python prometheus-client python-consul pygogo
     pip freeze > requirements.txt
     sed -i '/pkg-resources/d' requirements.txt
+
+# Run it manually
+
+    docker build . -t handbrakecli
+    docker run -it --rm -v /mnt/video/Television/The\ Simpsons/Season\ 1:/input handbrakecli bash
+    export HANDBRAKE_ENCODER="x265_10bit"
+    export HANDBRAKE_QUALITY="40.0"
+    export HANDBRAKE_VIDEO_BITRATE="10000"
+    python3 wrapper.py 'The Simpsons - S01E11 - The Crepes of Wrath WEBDL-1080p.mkv' 'The Simpsons - S01E11 - The Crepes of Wrath WEBDL-1080p.mkv' 'My 1080p'
+    docker run -it --rm -v /mnt/video/Movies/Harry\ Potter\ and\ the\ Prisoner\ of\ Azkaban\ \(2004\):/input handbrakecli bash
+    python3 wrapper.py 'Harry Potter and the Prisoner of Azkaban (2004) {imdb-tt0304141}.mkv' 'Harry Potter and the Prisoner of Azkaban (2004) {imdb-tt0304141}.mkv' 'My 1080p'
+
+    docker run -it --rm -v /mnt/video/Television/SpongeBob\ SquarePants/Season\ 1:/input handbrakecli bash
+    export HANDBRAKE_ENCODER="x265_10bit"
+    export HANDBRAKE_QUALITY="40.0"
+    export HANDBRAKE_VIDEO_BITRATE="10000"
+    python3 wrapper.py 'SpongeBob SquarePants - S01E02 - Reef Blower SDTV.mkv' 'SpongeBob SquarePants - S01E02 - Reef Blower SDTV.mkv' 'My 1080p'
+
+    docker run -it --rm -v /mnt/video/Movies/Bad\ Words\ \(2013\):/input handbrakecli bash
+    export HANDBRAKE_ENCODER="x265_10bit"
+    export HANDBRAKE_QUALITY="40.0"
+    export HANDBRAKE_VIDEO_BITRATE="10000"
+    python3 wrapper.py 'Bad Words (2013).mp4' 'Bad Words (2013).mp4' 'My 1080p'
---
+++ handbrakeOptionsGenerator/tests/test_handbrake_options_generator.py
@@ -0,0 +1,113 @@
+import pytest
+from mediainfo.mediainfo import Mediainfo
+from handbrakeOptionsGenerator.handbrake_options_generator import HandbrakeOptionGenerator
+import os
+import json
+from unittest import mock
+
+fixture_path = os.path.join(os.path.dirname(__file__), 'fixtures')
+
+
+@pytest.fixture
+def my_fixtures(fs):
+    fs.add_real_directory(fixture_path, target_path='/src')
+    yield fs
+
+
+def test_get_subtitle_flags(fs):
+    file_name = "harry_potter_prizoner.json"
+    file_path = '/src' + "/" + file_name
+    fs.add_real_file(fixture_path + "/" + file_name, target_path=file_path)
+    with open(file_path) as f:
+        mediainfo = Mediainfo(file_path)
+        mediainfo.set_mediainfo_json(json.load(f))
+    handbrake_options = HandbrakeOptionGenerator(mediainfo)
+    flags = handbrake_options.generate_subtitle_flags()
+    assert flags == ['-s', '1,2,3,4,5,6,7,8,9,10,11,12,13']
+
+
+def test_get_subtitle_flags_simpsons(fs):
+    file_name = "simpsons_s01e11.json"
+    file_path = '/src' + "/" + file_name
+    fs.add_real_file(fixture_path + "/" + file_name, target_path=file_path)
+    with open(file_path) as f:
+        mediainfo = Mediainfo(file_path)
+        mediainfo.set_mediainfo_json(json.load(f))
+    handbrake_options = HandbrakeOptionGenerator(mediainfo)
+    flags = handbrake_options.generate_subtitle_flags()
+    assert flags == []
+
+
+def test_get_codec_flag():
+    handbrake_options = HandbrakeOptionGenerator(mock.Mock(Mediainfo))
+    flag = handbrake_options.get_codec_flag('A_AAC-2')
+    assert flag == 'copy:aac'
+
+
+def test_get_audio_codec_flags():
+    mock_mediainfo = mock.Mock(Mediainfo)
+    mediainfo_attrs = {'get_audio_codec_ids.return_value': ['A_DTS', 'A_AC3', 'A_TRUEHD'],
+                       'get_audio_track_count.return_value': 3}
+    mock_mediainfo.configure_mock(**mediainfo_attrs)
+    handbrake_options = HandbrakeOptionGenerator(mock_mediainfo)
+    handbrake_options.get_audio_codec_flags()
+    flags = handbrake_options.audio_flags
+    assert flags == ['--aencoder', 'copy:ac3,copy:dts,copy:dtshd,copy:truehd']
+
+
+def test_get_audio_codec_flags_no_copy_matches():
+    mock_mediainfo = mock.Mock(Mediainfo)
+    mediainfo_attrs = {'get_audio_codec_ids.return_value': ['x'], 'get_audio_track_count.return_value': 1}
+    mock_mediainfo.configure_mock(**mediainfo_attrs)
+    handbrake_options = HandbrakeOptionGenerator(mock_mediainfo)
+    handbrake_options.get_audio_codec_flags()
+    flags = handbrake_options.audio_flags
+    assert flags == ['--audio-fallback', 'av_aac']
+
+
+def test_get_audio_codec_flags_duplicate_codecs():
+    mock_mediainfo = mock.Mock(Mediainfo)
+    mediainfo_attrs = {'get_audio_codec_ids.return_value': ['A_AC3', 'A_AC3'], 'get_audio_track_count.return_value': 2}
+    mock_mediainfo.configure_mock(**mediainfo_attrs)
+    handbrake_options = HandbrakeOptionGenerator(mock_mediainfo)
+    handbrake_options.get_audio_codec_flags()
+    flags = handbrake_options.audio_flags
+    assert flags == ['--aencoder', 'copy:ac3']
+
+
+def test_get_video_flags():
+    mock_mediainfo = mock.Mock(Mediainfo)
+    mediainfo_attrs = {'get_video_frame_rate.return_value': '99.234', 'get_video_frame_rate_mode.return_value': 'CFR'}
+    mock_mediainfo.configure_mock(**mediainfo_attrs)
+    handbrake_options = HandbrakeOptionGenerator(mock_mediainfo)
+    assert handbrake_options.generate_video_flags() == ['-r', '99.234', '--cfr']
+
+
+def test_get_audio_track_number_flags():
+    mock_mediainfo = mock.Mock(Mediainfo)
+    mediainfo_attrs = {'get_audio_track_count.return_value': 2}
+    mock_mediainfo.configure_mock(**mediainfo_attrs)
+    handbrake_options = HandbrakeOptionGenerator(mock_mediainfo)
+    handbrake_options.get_audio_track_number_flags()
+    flags = handbrake_options.audio_flags
+    assert flags == ['-a', '1,2']
+
+
+def test_get_audio_track_bitrates():
+    mock_mediainfo = mock.Mock(Mediainfo)
+    mediainfo_attrs = {'get_audio_tracks.return_value': [{'BitRate': '100'}, {'BitRate': '200'}]}
+    mock_mediainfo.configure_mock(**mediainfo_attrs)
+    handbrake_options = HandbrakeOptionGenerator(mock_mediainfo)
+    handbrake_options.get_audio_track_bitrates()
+    flags = handbrake_options.audio_flags
+    assert flags == ['--ab', '100,200']
+
+
+def test_get_audio_track_sample_rate():
+    mock_mediainfo = mock.Mock(Mediainfo)
+    mediainfo_attrs = {'get_audio_tracks.return_value': [{'SamplingRate': '48000'}, {'SamplingRate': '48000'}]}
+    mock_mediainfo.configure_mock(**mediainfo_attrs)
+    handbrake_options = HandbrakeOptionGenerator(mock_mediainfo)
+    handbrake_options.get_audio_track_sample_rate()
+    flags = handbrake_options.audio_flags
+    assert flags == ['--arate', 'auto,auto']
